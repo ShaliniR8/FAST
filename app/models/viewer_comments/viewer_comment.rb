daf417e6ecffa9206289a1d18ea088e876ad3f9a
Blair Li <blair.li@prodigiq.com>
2019-05-02 20:51:30 -0400

    Updated shared view files in shared and forms

--- app/models/viewer_comments/viewer_comment.rb
+++ app/models/viewer_comments/viewer_comment.rb
@@ -1,5 +1,16 @@
 class ViewerComment < ActiveRecord::Base
   belongs_to :viewer, foreign_key:"user_id",class_name:"User"
+
+  def self.get_meta_fields(*args)
+    visible_fields = (args.empty? ? ['index', 'form', 'show'] : args)
+    [
+      {field: 'user_id',    title: 'Submitter',    num_cols: 6,   type: 'user',      visible: 'index,show',       required: false},
+      {field: 'created_at', title: 'Submit Date',  num_cols: 6,   type: 'datetime',  visible: 'index,show',       required: true},
+      {field: 'content',    title: 'Content',      num_cols: 12,  type: 'textarea',  visible: 'index,form,show',  required: true},
+    ].select{|f| (f[:visible].split(',') & visible_fields).any?}
+  end
+
+
   def get_content
     self.content.gsub(/\n/, '<br/>').html_safe
   end
