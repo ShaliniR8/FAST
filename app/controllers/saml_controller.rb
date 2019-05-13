a0247d9aeac1b4cd35efd4ff249d50ffe70fee82
Dylan Januzik <dylan.januzik@prodigiq.com>
2019-05-13 17:16:52 -0400

    can now log in using IdP; revisions to initialize SAML from config

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -7,22 +7,38 @@ class SamlController < ApplicationController
   end
 
   def consume
-    # response = OneLogin::RubySaml::Response.new(params[:SAMLResponse], settings: saml_settings)
+    response = OneLogin::RubySaml::Response.new(params[:SAMLResponse], settings: saml_settings)
 
-    #Validate the SAML Response and check if user exists in system
-    # if response.is_valid?
-    #   # session[:nameid] = response.nameid
-    #   Rails.logger.debug response.nameid
-    #   session[:user_id] = User.where(email: response.nameid).first.id
-    #   session[:mode]=""
-    #   session[:last_active] = Time.now
-    #   redirect_to_target_or_default(root_url)
-    #   # session[:user_id] = 1 #TODO: Parse response for account email and match with existing account
-    #   # session[:privileges] = {} #TODO: Generate list of permissions based on what they provide in the response
-    # else
-    #   authorize_failure
-    # end
+    # Validate the SAML Response and check if user exists in system
+    if response.is_valid?
+      user = digest_response response
+      session[:user_id] = user.id
+      session[:mode]=""
+      session[:last_active] = Time.now
+      redirect_to_target_or_default(root_url)
+    else
+      redirect_to access_point
+      # authorize_failure
+    end
   end
+
+#PLACE IN CONFIG UNDER SSO:
+  def digest_response(response)
+    #Unique digest statement to find user-identifying email from IdP:
+    email = response.attributes[:'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress']
+    user = User.where(email: email).first
+    if user.nil?
+      Rails.logger.info "SSO ERROR: Could not find user with Email address #{email}"
+      redirect_to access_point
+    else
+      user
+    end
+  end
+
+  def access_point
+    'https://syextec0002.suncountry.com/adfs/ls/idpinitiatedsignon'
+  end
+#END PLACE IN CONFIG UNDER SSO
 
   def logout
     if params[:SAMLRequest]
@@ -93,91 +109,41 @@ class SamlController < ApplicationController
   def metadata
     settings = saml_settings
     meta = OneLogin::RubySaml::Metadata.new
-    # render xml: meta.generate(settings), content_type: 'application/samlmetadata+xml'
     render xml: meta.generate(settings, true)
   end
 
   def saml_settings
-    #Always ask if they have a url that will provide their metadata
-    # if they do, you can generate the majority of the settings from the metadata_parser:
-    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-    settings = idp_metadata_parser.parse_remote('https://syextec0002.suncountry.com/FederationMetadata/2007-06/FederationMetadata.xml')
-    # If you can't use that, you will have to manually define values after the ###IdP Info### Tag
+    saml_data = Object.const_get("#{BaseConfig.airline_code}_Config")::SAML_DATA
+    Rails.logger.debug saml_data
+    if saml_data[:metadata_link].present?
+      idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+      settings = idp_metadata_parser.parse_remote(saml_data[:metadata_link])
+    else
+      settings = OneLogin::RubySaml::Settings.new
+      settings.idp_entity_id      = saml_data[:idp_entity_id]
+      settings.idp_sso_target_url = saml_data[:idp_sso_target_url]
+      settings.idp_slo_target_url = saml_data[:idp_slo_target_url]
 
-    #The current path to ProSafeT:
-    sp_url = "#{request.protocol}#{request.host_with_port}"  #'http://198.71.58.173:3002/' #'http://smx.prosafet.com'
+      if saml_data[:user_fingerprint]
+        settings.idp_cert_fingerprint           = saml_data[:idp_cert_fingerprint]
+        settings.idp_cert_fingerprint_algorithm = saml_data[:idp_cert_fingerprint_algorithm]
+      else
+        if saml_data[:encryption_cert].present?
+          settings.idp_cert = saml_data[:signing_cert]
+        else
+          idp_cert_multi = {
+            signing:  ["-----BEGIN CERTIFICATE----- #{saml_data[:signing_cert]} -----END CERTIFICATE-----"],
+            encryption: ["-----BEGIN CERTIFICATE----- #{saml_data[:encryption_cert]} -----END CERTIFICATE-----"]
+          }
+        end
+      end
+    end
 
-    #Route for app to interpret IdP response:
-    settings.assertion_consumer_service_url = "#{sp_url}/saml/consume"
-
-    #Route to provide xml metadata file dynamically
-    settings.issuer                         = "#{sp_url}/saml/metadata"
-
-    settings.assertion_consumer_logout_service_url = "#{sp_url}/saml/logout"
-
-    settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-
-    ###IdP Info###
-
-    #Route to IdP; should be in metadata.xml they provide under <EntityDescriptor ... entityID="|__this__|" />
-    # settings.idp_entity_id                  = "https://SYEXTEC0001.suncountry.com/adfs/services/trust"
-
-    #Route to IdP's sign-in; should be in metadata.xml they provide under <SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="|__this__|" />
-    # settings.idp_sso_target_url             = "https://syextec0001.suncountry.com/adfs/ls/"
-
-    #Route to IdP's sign-out; should be in metadata.xml they provide under <SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="|__this__|" />
-    # settings.idp_slo_target_url             = "https://syextec0001.suncountry.com/adfs/ls/"
-
-    # #Specifies the hash fingerprint of the certificate.
-    # #It is recommended that you only define the actual certificate below under idp_cert rather than using collision-risk fingerprints
-    # settings.idp_cert_fingerprint           = OneLoginAppCertFingerPrint
-
-    # #Specifies the route to the hash algorithm- standard format is sha1, so leave as "http://www.w3.org/2000/09/xmldsig#sha1"
-    # settings.idp_cert_fingerprint_algorithm = "http://www.w3.org/2000/09/xmldsig#sha1"
-
-    # #Specifies the formatting of the identifier
-    # settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-
-    #In place of the fingerprint, can also provide the whole certificate- the fingerprint is more efficient however.
-    #IdP's certificate; should be in metadata.xml
-    # settings.idp_cert                       = ''
-
-    #If there are two certificates, then please place them appropriately in the following hash (match signing to signing and encryption to encryption)
-    # idp_cert_multi = {
-    #   signing:  ['-----BEGIN CERTIFICATE-----
-    #   MIIC8DCCAdigAwIBAgIQHXFgSsDwt7JHI7Qtyk1zUjANBgkqhkiG9w0BAQsFADA0MTIwMAYD
-    #   VQQDEylBREZTIFNpZ25pbmcgLSBTWUVYVEVDMDAwMS5zdW5jb3VudHJ5LmNvbTAeFw0xNjEw
-    #   MTIxNDE3MDBaFw0xOTEwMTIxNDE3MDBaMDQxMjAwBgNVBAMTKUFERlMgU2lnbmluZyAtIFNZ
-    #   RVhURUMwMDAxLnN1bmNvdW50cnkuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
-    #   AQEA4tUOjmp4Vfsxbf/u0G/sYXkjQ2edUE3A1BqZSA/LT0TCq4czSQCEwaoZcAL5juwKrtIt
-    #   XxX6XeXaXggpAjgjrxpircCRJzsUdNAcbPJtWDP6tE0c6OXBaYgGhoxIZOPhv8Ohh9iyF/k9
-    #   EVMazZayz4QgTKBTD254RVxGprmwTd7F1oeiVfigESSViDA5ErN3BRGOppqWZ3c6U4XqNieA
-    #   59dle3bFqv0oS0bmoU8dx/RQXNgdHCRXTJBatPs4Q45am1had9IJwzcZXajwDo8kZC+f9usZ
-    #   d0bHGd8vC32FZLtcF38nNoV9gwXgdqVEfwd8Cb5GMK3KclyRd8cXTXhCAQIDAQABMA0GCSqG
-    #   SIb3DQEBCwUAA4IBAQCY7Dqjv3pHjhqf6TI55t6PWwkXHtBgutsGlZAdekFvKOsKjScspoDA
-    #   N5cqV1akRq4v7Zy+ib6wE2uLC+8Ry4jBv1Yo+bSXz+9z4h7MVvXPCQiW30bL/OMi6/XtctLB
-    #   HywVNzCcqissN9ymGzsRxXnvUmoiOovbVpDG6MDgBBBsGn1g4KOkdyT83iqqYlLrtjXm/ruA
-    #   qjLG8tUlU1NyIhXKzpcFYI9gc3AMDS4vXIOAzW2SBiXzxfIHXFkT8u9RnYQ4dG1uQtMTVGT8
-    #   82TkkGgfMvJtJv7zAm4CXk+4qLzoOb+Zl5NoswPfe9GSwGk880uucA6MG5kqPubw6HgJAHLi
-    #   -----END CERTIFICATE-----'],
-    #   encryption: ['-----BEGIN CERTIFICATE-----
-    #   MIIC9jCCAd6gAwIBAgIQKQFFKDf7WJRKo/DuHm/DTzANBgkqhkiG9w0BAQsFADA3MTUwMwYD
-    #   VQQDEyxBREZTIEVuY3J5cHRpb24gLSBTWUVYVEVDMDAwMS5zdW5jb3VudHJ5LmNvbTAeFw0x
-    #   NjExMjAxNjU3MDFaFw0xOTExMjAxNjU3MDFaMDcxNTAzBgNVBAMTLEFERlMgRW5jcnlwdGlv
-    #   biAtIFNZRVhURUMwMDAxLnN1bmNvdW50cnkuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
-    #   MIIBCgKCAQEA3QkCaMeyXDSxiuQ23MeUfLOkuMld9rm39rNaTtZL40Xzjlw55HWP78tYoEIE
-    #   qzAxciwTyDVX3RDbhy9cFavT8cifcRNNUx5iF4kFD+KOxrtB3IU3aocHPJNxJxzgF854rDFR
-    #   ZFzYwyUxC/rbmgI1HbUDbXVinPbg15iUACZGaReMP77n8mdEWLWzkDnd0Ef4MsBP45m+l8SD
-    #   UchORw9R7FpRGZ+VoIEhxwSI7/hdlBQXDpk6TfCEhhwXKt4wjYeLFhqaOfaPXREpXzdM0u1i
-    #   CE24OREHC/zcB1SuGK8aANAm2gYQ47N1XWXbIDP3cFwCIPXVo1L6blM7T7AZ+qfg+QIDAQAB
-    #   MA0GCSqGSIb3DQEBCwUAA4IBAQC0PdDA8IfcFYcGyOJKniZEsegQP2EORFTA6F1ONGDfB8hJ
-    #   WzxGa2072cqtxLljH6baeJqr9rvMQDxYGs99ZjA2z5Uv2mSSu70aJo40DAdhfaPWUpoetQMw
-    #   IHBr83l1+JFGgK973JjvEDHpanupkgAC8oWuMdAZDmt9eF8jAdR468MTxY+ySb6b+nMcUet8
-    #   jNWJgOQyOxGVdSz3ixGaev8Q3b1Fzr41xgS+t7TClUUwlSOOiRBr0e37nWlq6BbgwrsHC9A1
-    #   xUm1AVJ4Hl9/CqaSpV95RGQFyA5dDEDyHZ+Uk7qtg2Z6Xluqowhc3mpPaBNY3jzO8pSkqPkI
-    #   JcAE5s1p
-    #   -----END CERTIFICATE-----']
-    # }
+    sp_url = "#{request.protocol}#{request.host_with_port}"
+    settings.assertion_consumer_service_url         = "#{sp_url}#{saml_data[:response_consume_url]}"
+    settings.issuer                                 = "#{sp_url}#{saml_data[:issuer_metadata_url]}"
+    settings.assertion_consumer_logout_service_url  = "#{sp_url}#{saml_data[:issuer_logout_url]}"
+    settings.name_identifier_format                 = saml_data[:name_id_format]
 
     # # Optional for most SAML IdPs
     # settings.authn_context = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
@@ -199,7 +165,7 @@ class SamlController < ApplicationController
     settings.security[:digest_method] = XMLSecurity::Document::SHA1
     settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
 
-    Rails.logger.debug settings.inspect
+    # Rails.logger.debug settings.inspect
 
     settings
   end
--- app/models/systems/scx_config.rb
+++ app/models/systems/scx_config.rb
@@ -21,7 +21,7 @@ class SCX_Config
       :has_mobile_app                                 => false,
       :enable_mailer                                  => true,
       :time_zone                                      => 'Central Time (US & Canada)',
-
+      :enable_sso                                     => true,
 
       # Safety Reporting Module
       :submission_description                         => true,
@@ -242,6 +242,57 @@ MATRIX_INFO = {
   }
 }
 
+SAML_DATA = {
+  # Always ask if they have a URL for their metadata
+  # If they do, you can skip all tags under IdP Info and use the following- otherwise leave this string empty: ''
+    metadata_link: 'https://syextec0002.suncountry.com/FederationMetadata/2007-06/FederationMetadata.xml',
+
+  ### IdP Info ###
+
+    # Route to IdP; should be in metadata.xml under:
+     # <EntityDescriptor ... entityID="|__this__|" />
+    idp_entity_id: 'https://SYEXTEC0001.suncountry.com/adfs/services/trust',
+
+    # Route to IdP's sign-in; should be in metadata.xml under:
+     # <SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="|__this__|" />
+    idp_sso_target_url: 'https://syextec0001.suncountry.com/adfs/ls/',
+
+    # Route to IdP's sign-out; should be in metadata.xml under:
+     # <SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="|__this__|" />
+    idp_slo_target_url: "https://syextec0001.suncountry.com/adfs/ls/",
+    # Specifies the route to the hash algorithm; standard format is sha1, s"http://www.w3.org/2000/09/xmldsig#sha1"
+
+    ### Fingerprint Config ###
+
+      # Fingerprint is less safe than the full certificate- only set to true and provide settings if needed
+      use_fingerprint: false,
+
+      # Value of fingerprint; should be in metadata.xml
+      idp_cert_fingerprint: '',
+
+      # Route to hash algorithm- standard is SHA1, you shouldn't have to alter this:
+      idp_cert_fingerprint_algorithm: 'http://www.w3.org/2000/09/xmldsig#sha1',
+
+    ### Certificate Config ###
+      # This will be used in place of the fingerprint and is more secure
+      # If only on certificate, place it under signing_cert
+      # Do NOT include BEGIN-END tags on the certificates
+
+      # Certificate used for signing the response
+      signing_cert:     'MIIC8DCCAdigAwIBAgIQHXFgSsDwt7JHI7Qtyk1zUjANBgkqhkiG9w0BAQsFADA0MTIwMAYDVQQDEylBREZTIFNpZ25pbmcgLSBTWUVYVEVDMDAwMS5zdW5jb3VudHJ5LmNvbTAeFw0xNjEwMTIxNDE3MDBaFw0xOTEwMTIxNDE3MDBaMDQxMjAwBgNVBAMTKUFERlMgU2lnbmluZyAtIFNZRVhURUMwMDAxLnN1bmNvdW50cnkuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4tUOjmp4Vfsxbf/u0G/sYXkjQ2edUE3A1BqZSA/LT0TCq4czSQCEwaoZcAL5juwKrtItXxX6XeXaXggpAjgjrxpircCRJzsUdNAcbPJtWDP6tE0c6OXBaYgGhoxIZOPhv8Ohh9iyF/k9EVMazZayz4QgTKBTD254RVxGprmwTd7F1oeiVfigESSViDA5ErN3BRGOppqWZ3c6U4XqNieA59dle3bFqv0oS0bmoU8dx/RQXNgdHCRXTJBatPs4Q45am1had9IJwzcZXajwDo8kZC+f9usZd0bHGd8vC32FZLtcF38nNoV9gwXgdqVEfwd8Cb5GMK3KclyRd8cXTXhCAQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQCY7Dqjv3pHjhqf6TI55t6PWwkXHtBgutsGlZAdekFvKOsKjScspoDAN5cqV1akRq4v7Zy+ib6wE2uLC+8Ry4jBv1Yo+bSXz+9z4h7MVvXPCQiW30bL/OMi6/XtctLBHywVNzCcqissN9ymGzsRxXnvUmoiOovbVpDG6MDgBBBsGn1g4KOkdyT83iqqYlLrtjXm/ruAqjLG8tUlU1NyIhXKzpcFYI9gc3AMDS4vXIOAzW2SBiXzxfIHXFkT8u9RnYQ4dG1uQtMTVGT882TkkGgfMvJtJv7zAm4CXk+4qLzoOb+Zl5NoswPfe9GSwGk880uucA6MG5kqPubw6HgJAHLi',
+      # Certificate used for encrypting the response
+      encryption_cert:  'MIIC9jCCAd6gAwIBAgIQKQFFKDf7WJRKo/DuHm/DTzANBgkqhkiG9w0BAQsFADA3MTUwMwYDVQQDEyxBREZTIEVuY3J5cHRpb24gLSBTWUVYVEVDMDAwMS5zdW5jb3VudHJ5LmNvbTAeFw0xNjExMjAxNjU3MDFaFw0xOTExMjAxNjU3MDFaMDcxNTAzBgNVBAMTLEFERlMgRW5jcnlwdGlvbiAtIFNZRVhURUMwMDAxLnN1bmNvdW50cnkuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3QkCaMeyXDSxiuQ23MeUfLOkuMld9rm39rNaTtZL40Xzjlw55HWP78tYoEIEqzAxciwTyDVX3RDbhy9cFavT8cifcRNNUx5iF4kFD+KOxrtB3IU3aocHPJNxJxzgF854rDFRZFzYwyUxC/rbmgI1HbUDbXVinPbg15iUACZGaReMP77n8mdEWLWzkDnd0Ef4MsBP45m+l8SDUchORw9R7FpRGZ+VoIEhxwSI7/hdlBQXDpk6TfCEhhwXKt4wjYeLFhqaOfaPXREpXzdM0u1iCE24OREHC/zcB1SuGK8aANAm2gYQ47N1XWXbIDP3cFwCIPXVo1L6blM7T7AZ+qfg+QIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQC0PdDA8IfcFYcGyOJKniZEsegQP2EORFTA6F1ONGDfB8hJWzxGa2072cqtxLljH6baeJqr9rvMQDxYGs99ZjA2z5Uv2mSSu70aJo40DAdhfaPWUpoetQMwIHBr83l1+JFGgK973JjvEDHpanupkgAC8oWuMdAZDmt9eF8jAdR468MTxY+ySb6b+nMcUet8jNWJgOQyOxGVdSz3ixGaev8Q3b1Fzr41xgS+t7TClUUwlSOOiRBr0e37nWlq6BbgwrsHC9A1xUm1AVJ4Hl9/CqaSpV95RGQFyA5dDEDyHZ+Uk7qtg2Z6Xluqowhc3mpPaBNY3jzO8pSkqPkIJcAE5s1p',
+  ### END IdP Info ###
+
+  ### Base Access Routes for this implementation ### - You shouldn't have to alter these
+    response_consume_url: '/saml/consume',
+    issuer_metadata_url:  '/saml/metadata',
+    issuer_logout_url:    '/saml/logout',
+    name_id_format:       "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
+
+  # Security
+}
+
 #ALL FOLLOWING MAY NEED CORRECTION/REVISION
 
   # Calculate the severity based on #{BaseConfig.airline[:code]}'s risk matrix
@@ -275,5 +326,7 @@ MATRIX_INFO = {
       return MATRIX_INFO[:risk_table_index][lookup_table[probability_score][severity_score].to_sym]
     end
   end
+
+
 
 end
