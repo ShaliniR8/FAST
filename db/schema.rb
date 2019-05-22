695d2f74bc98c89221253a36be7d4e1dd9358f0f
Blair Li <blair.li@prodigiq.com>
2019-05-22 19:27:32 -0400

    server base url change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20190517173316) do
+ActiveRecord::Schema.define(:version => 20190522203544) do
 
   create_table "access_controls", :force => true do |t|
     t.boolean "list_type"
@@ -870,6 +870,17 @@ ActiveRecord::Schema.define(:version => 20190517173316) do
     t.integer "poc_id"
   end
 
+  create_table "private_links", :force => true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "digest"
+    t.date     "expire_date"
+    t.string   "access_level"
+    t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "privileges", :force => true do |t|
     t.string   "name"
     t.text     "description"
@@ -1160,6 +1171,16 @@ ActiveRecord::Schema.define(:version => 20190517173316) do
     t.datetime "updated_at"
   end
 
+  create_table "signatures", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "signee_name"
+    t.integer  "user_id"
+    t.string   "owner_id"
+    t.string   "owner_type"
+    t.string   "path"
+  end
+
   create_table "sms_actions", :force => true do |t|
     t.string   "title"
     t.string   "responsible_department"
@@ -1348,6 +1369,7 @@ ActiveRecord::Schema.define(:version => 20190517173316) do
     t.integer  "owner_obj_id"
     t.string   "poc_first_name"
     t.string   "poc_last_name"
+    t.string   "alt_user"
   end
 
   create_table "users", :force => true do |t|
@@ -1400,6 +1422,7 @@ ActiveRecord::Schema.define(:version => 20190517173316) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "alt_user"
   end
 
 end
