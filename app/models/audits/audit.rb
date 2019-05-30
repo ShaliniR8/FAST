3eeeac611096e8abb15e959925333b420be15524
Samir Matin <samir.matin@prodigiq.com>
2019-05-29 20:32:58 -0400

    Audit now accepts nested attributes for checklists.
    This was done so that an audit's checklists can be updated when updating an audit, utilized in the ProSafeT mobile app.

--- app/models/audits/audit.rb
+++ app/models/audits/audit.rb
@@ -29,6 +29,7 @@ class Audit < ActiveRecord::Base
   accepts_nested_attributes_for :requirements
   accepts_nested_attributes_for :comments
   accepts_nested_attributes_for :checklist_records, :allow_destroy => true
+  accepts_nested_attributes_for :checklists
 
   after_create -> { create_transaction('Create') }
   # after_update -> { create_transaction('Edit') }
