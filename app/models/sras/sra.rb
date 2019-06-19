636b42f952a7ac21a95f02f4930a14c9c8274d85
Trevor Ryles <trevor.ryles@prodigiq.com>
2019-06-18 20:22:07 -0400

    adds sra field configurations for bsk

--- app/models/sras/sra.rb
+++ app/models/sras/sra.rb
@@ -69,48 +69,8 @@ class Sra < ActiveRecord::Base
 
   def self.get_meta_fields(*args)
     visible_fields = (args.empty? ? ['index', 'form', 'show'] : args)
-    return [
-      { field: "get_id",                    title: "ID",                                       num_cols: 6,   type: "text",        visible: 'index,show',        required: false},
-      { field: "status",                    title: "Status",                                   num_cols: 6,   type: "text",        visible: 'index,show',        required: false},
-      { field: 'get_source',                title: 'Source of Input',                           num_cols: 6,   type: 'text',         visible: 'index,show',      required: false},
-      {                                                                                                       type: "newline",     visible: 'form,show'},
-      { field: "title",                     title: "SRA Title",                                num_cols: 6,   type: "text",        visible: 'index,form,show',   required: false},
-      { field: "type_of_change",            title: "Type of Change",                           num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: get_custom_options('SRA Type of Change')},
-      { field: "system_task",               title: "System/Task",                              num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: get_custom_options('Systems/Tasks')},
-      { field: "responsible_user_id",       title: "Responsible User",                         num_cols: 6,   type: "user",        visible: 'index,form,show',  required: false},
-      { field: "reviewer_id",               title: "Quality Reviewer",                         num_cols: 6,   type: "user",        visible: 'form,show',         required: false},
-      { field: "approver_id",               title: "Final Approver",                           num_cols: 6,   type: "user",        visible: 'form,show',         required: false},
-      { field: "scheduled_completion_date", title: "Scheduled Completion Date",                num_cols: 6,   type: "date",        visible: 'index,form,show',   required: false},
-      { field: "current_description",       title: "Describe the Current System",              num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
-      { field: "plan_description",          title: "Describe Proposed Plan",                   num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
-      {                                     title: "Affected Department",                      num_cols: 12,  type: "panel_start", visible: 'form,show'},
-      { field: "departments",               title: "Affected Departments",                     num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: get_custom_options('Departments')},
-      { field: "other_department",          title: "Other Affected Departments",               num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
-      { field: "departments_comment",       title: "Affected Departments Comments",            num_cols: 12,  type: "textarea",    visible: 'form,show',        required: false},
-      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
-
-      {                                     title: "Affected Programs",                        num_cols: 12,  type: "panel_start", visible: 'form,show'},
-      { field: "programs",                  title: "Affected Programs",                        num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: get_custom_options('Programs')},
-      { field: "other_program",             title: "Other Affected Programs",                  num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
-      { field: "programs_comment",          title: "Affected Programs Comments",               num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
-      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
-
-      {                                     title: "Affected Manuals",                         num_cols: 12,  type: "panel_start", visible: 'form,show'},
-      { field: "manuals",                   title: "Affected Manuals",                         num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: get_custom_options('Manuals')},
-      { field: "other_manual",              title: "Other Affected Manuals",                   num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
-      { field: "manuals_comment",           title: "Affected Manuals Comments",                num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
-      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
-
-      {                                     title: "Affected #{Sra.sra_section_4}",           num_cols: 12,  type: "panel_start", visible: 'form,show',},
-      { field: "compliances",               title: "Affected #{Sra.sra_section_4}",           num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: get_custom_options("#{Sra.sra_section_4}")},
-      { field: "other_compliance",          title: "Other Affected #{Sra.sra_section_4}",     num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
-      { field: "compliances_comment",       title: "Affected #{Sra.sra_section_4} Comments",  num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
-      {                                                                                       num_cols: 12,  type: "panel_end",   visible: 'form,show'},
-
-      { field: "closing_comment",           title: "Responsible User's Closing Comments",      num_cols: 12,  type: "text",        visible: 'show'},
-      { field: "reviewer_comment",          title: "Quality Reviewer's Closing Comments",      num_cols: 12,  type: "text",        visible: 'show'},
-      { field: "approver_comment",          title: "Final Approver's Closing Comments",        num_cols: 12,  type: "text",        visible: 'show'},
-    ].select{|f| (f[:visible].split(',') & visible_fields).any?}
+    meta_fields = BaseConfig.get_sra_meta_fields
+    meta_fields.select{|f| (f[:visible].split(',') & visible_fields).any?}
   end
 
   def get_source
@@ -122,16 +82,6 @@ class Sra < ActiveRecord::Base
       "<b style='color:grey'>N/A</b>".html_safe
     end
   end
-
-
-  def self.sra_section_4
-    if BaseConfig.airline_code == 'BSK'
-      'System Task Analysis SHEL(L) Models'
-    else
-      'Regulatory Compliances'
-    end
-  end
-
 
   def self.get_custom_options(title)
     CustomOption
--- app/models/systems/bsk_config.rb
+++ app/models/systems/bsk_config.rb
@@ -41,7 +41,50 @@ class BSK_Config
     }
   end
 
+  def self.get_sra_meta_fields
+    [
+      { field: "get_id",                    title: "ID",                                       num_cols: 6,   type: "text",        visible: 'index,show',        required: false},
+      { field: "status",                    title: "Status",                                   num_cols: 6,   type: "text",        visible: 'index,show',        required: false},
+      { field: 'get_source',                title: 'Source of Input',                           num_cols: 6,   type: 'text',         visible: 'index,show',      required: false},
+      {                                                                                                       type: "newline",     visible: 'form,show'},
+      { field: "title",                     title: "SRA Title",                                num_cols: 6,   type: "text",        visible: 'index,form,show',   required: false},
+      { field: "type_of_change",            title: "Type of Change",                           num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: Sra.get_custom_options('SRA Type of Change')},
+      { field: "system_task",               title: "System/Task",                              num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: Sra.get_custom_options('Systems/Tasks')},
+      { field: "responsible_user_id",       title: "Responsible User",                         num_cols: 6,   type: "user",        visible: 'index,form,show',  required: false},
+      { field: "reviewer_id",               title: "Quality Reviewer",                         num_cols: 6,   type: "user",        visible: 'form,show',         required: false},
+      { field: "approver_id",               title: "Final Approver",                           num_cols: 6,   type: "user",        visible: 'form,show',         required: false},
+      { field: "scheduled_completion_date", title: "Scheduled Completion Date",                num_cols: 6,   type: "date",        visible: 'index,form,show',   required: false},
+      { field: "current_description",       title: "Describe the Current System",              num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
+      { field: "plan_description",          title: "Notes",                                    num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
+      {                                     title: "Affected Department",                      num_cols: 12,  type: "panel_start", visible: 'form,show'},
+      { field: "departments",               title: "Affected Departments",                     num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: Sra.get_custom_options('Departments')},
+      { field: "other_department",          title: "Other Affected Departments",               num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
+      { field: "departments_comment",       title: "Affected Departments Comments",            num_cols: 12,  type: "textarea",    visible: 'form,show',        required: false},
+      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
 
+      {                                     title: "Affected Programs",                        num_cols: 12,  type: "panel_start", visible: 'form,show'},
+      { field: "programs",                  title: "Affected Programs",                        num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: Sra.get_custom_options('Programs')},
+      { field: "other_program",             title: "Other Affected Programs",                  num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
+      { field: "programs_comment",          title: "Affected Programs Comments",               num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
+      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
+
+      {                                     title: "Affected Manuals",                         num_cols: 12,  type: "panel_start", visible: 'form,show'},
+      { field: "manuals",                   title: "Affected Manuals",                         num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: Sra.get_custom_options('Manuals')},
+      { field: "other_manual",              title: "Other Affected Manuals",                   num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
+      { field: "manuals_comment",           title: "Affected Manuals Comments",                num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
+      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
+
+      {                                     title: "Affected System Task Analysis SHEL(L) Models",           num_cols: 12,  type: "panel_start", visible: 'form,show',},
+      { field: "compliances",               title: "Affected System Task Analysis SHEL(L) Models",           num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: Sra.get_custom_options("System Task Analysis SHEL(L) Models")},
+      { field: "other_compliance",          title: "Other Affected System Task Analysis SHEL(L) Models",     num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
+      { field: "compliances_comment",       title: "Affected System Task Analysis SHEL(L) Models Comments",  num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
+      {                                                                                       num_cols: 12,  type: "panel_end",   visible: 'form,show'},
+
+      { field: "closing_comment",           title: "Responsible User's Closing Comments",      num_cols: 12,  type: "text",        visible: 'show'},
+      { field: "reviewer_comment",          title: "Quality Reviewer's Closing Comments",      num_cols: 12,  type: "text",        visible: 'show'},
+      { field: "approver_comment",          title: "Final Approver's Closing Comments",        num_cols: 12,  type: "text",        visible: 'show'},
+    ]
+  end
   OBSERVATION_PHASES = [
     "Observation Phase",
     "Condition",
