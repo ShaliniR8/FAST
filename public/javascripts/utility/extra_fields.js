c271b287b4076546a0260be215d2f977b3a333dc
Blair Li <blair.li@prodigiq.com>
2019-08-08 12:11:05 -0400

    Fixes issue with required field not working on Smart Form

--- public/javascripts/utility/extra_fields.js
+++ public/javascripts/utility/extra_fields.js
@@ -1,55 +1,62 @@
 $(document).ready(function(){
 
-	var submit_btn;
+  var submit_btn;
 
-	$(":submit").click(function(){
-		submit_btn = $(this).val();
-	});
+  $(":submit").click(function(){
+    submit_btn = $(this).val();
+  });
 
-	$("form").on('submit',function(event){
-		if(submit_btn == "Save for Later"){
-			buttons.removeAttr('data-disable-with');
-		}else{
+  $("form").on('submit',function(event){
+    if(submit_btn == "Save for Later"){
+      // buttons.removeAttr('data-disable-with');
+    }else{
 
-      $(".required_field").each(function(){
+      $(this).find('.required_field').each(function(){
         if($(this).closest("div.nested_field").css("display") == "none"){
-          $(this).removeClass("required_field");
+          $(this).switchClass("required_field", "required_field_hidden")
         }
       });
 
-			var error = false;
-			var result = true;
-			$(".required_field").css('border-color', '');
-			$(this).find('.required_field').each(function(){
-				if ($(this).val() == ""){
-					error = true;
-					result = false;
-					$(this).css('border-color', 'red');
-					event.preventDefault();
-				}
-			});
-			if (error){
+      $(this).find('.required_field_hidden').each(function(){
+        if($(this).closest("div.nested_field").css("display") != "none"){
+          $(this).switchClass("required_field_hidden", "required_field")
+        }
+      });
+
+      var error = false;
+      var result = true;
+
+      $(".required_field").css('border-color', '');
+      $(this).find('.required_field').each(function(){
+        if ($(this).val() == ""){
+          error = true;
+          result = false;
+          $(this).css('border-color', 'red');
+          event.preventDefault();
+        }
+      });
+      if (error){
         swal({
           title: "Error",
           text: "Please fill in all required fields.",
           type: "error",
           customClass: 'custom-swal',
         }).catch(swal.noop);
-				return result;
-			}
-		}
-	});
+        return result;
+      }
+    }
+  });
 
 
-	$(".non_dup").on('change',function(){
-		new_val=$(this).val();
-		uid=$(this).attr('id')
-		$(".non_dup").each(function(){
-			if ($(this).val() == new_val&&$(this).attr('id')!=uid){
-				 $(this).val('');
-			}
-		});
-	});
+  $(".non_dup").on('change',function(){
+    new_val=$(this).val();
+    uid=$(this).attr('id')
+    $(".non_dup").each(function(){
+      if ($(this).val() == new_val&&$(this).attr('id')!=uid){
+         $(this).val('');
+      }
+    });
+  });
 });
 
 
