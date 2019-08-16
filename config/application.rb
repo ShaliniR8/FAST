62adb690e1a018e605b1f1f4e1157289e9b65078
Dylan Januzik <dylan.januzik@prodigiq.com>
2019-08-16 17:11:29 -0400

    adjusts more files; removes trackings model

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module PrdgSession
   class Application < Rails::Application
   config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+  config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}', '{**}')]
   config.autoload_paths << "#{config.root}/lib"    # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
