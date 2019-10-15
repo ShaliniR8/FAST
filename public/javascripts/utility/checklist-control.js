8c86032d204dd1e75425d284531c0e940897a4e1
Samir Matin <samir.matin@prodigiq.com>
2019-10-15 21:29:08 +0000

    Added dirty form submit to checklists (see description)
    To allow a form to submit only changed fields, add the class 'dirty-submit' to the form.
    
    If the form dynamically sets the value of hidden input fields using val(), ensure that change() is also triggered.
    
    If the form dynamically adds fields, ensure that the class 'changed' is added to all input fields (including hidden) that were added.

--- public/javascripts/utility/checklist-control.js
+++ public/javascripts/utility/checklist-control.js
@@ -1,18 +1,23 @@
-$(function(){
-  $("#checklist_rows").on("click", '.header_toggle_btn', function(e){
-    e.preventDefault();
-    if($(this).hasClass("btn-default")){
-      $(this).closest('td').find('#is_header').val(true);
-      $(this).removeClass("btn-default").addClass("btn-success");
-    }else{
-      $(this).closest('td').find('#is_header').val(false);
-      $(this).removeClass("btn-success").addClass("btn-default");
+$(function() {
+  $('#checklist_rows').on('click', '.header_toggle_btn', function(event) {
+    event.preventDefault()
+    if ($(this).hasClass('btn-default')) {
+      var hiddenHeaderInput = $(this).closest('td').find('#is_header')
+      hiddenHeaderInput.val(true)
+      hiddenHeaderInput.change()
+      $(this).closest('td').find('#is_header').val(true)
+      $(this).removeClass('btn-default').addClass('btn-success')
+    } else {
+      $(this).closest('td').find('#is_header').val(false)
+      $(this).removeClass('btn-success').addClass('btn-default')
     }
-  });
+  })
 
-  $("#checklist_rows").on("click", '.remove_btn', function(e){
-    e.preventDefault();
-    $(this).closest("td").find("#delete_row").val(true);
-    $(this).closest(".to_delete").hide();
-  });
-});
+  $('#checklist_rows').on('click', '.remove_btn', function(event) {
+    event.preventDefault()
+    var hiddenDeleteInput = $(this).closest('td').find('#delete_row')
+    hiddenDeleteInput.val(true)
+    hiddenDeleteInput.change()
+    $(this).closest('.to_delete').hide()
+  })
+})
