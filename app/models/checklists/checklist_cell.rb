dbf94c2d31dabceab076a5a4f68666fedc8a585c
Samir Matin <samir.matin@prodigiq.com>
2019-10-22 22:53:13 +0000

    Fixes N+1 queries when rendering checklists on Audit show

--- app/models/checklists/checklist_cell.rb
+++ app/models/checklists/checklist_cell.rb
@@ -1,6 +1,6 @@
 # Checklists V3
 class ChecklistCell < ActiveRecord::Base
-
+  default_scope joins(:checklist_header_item).order('checklist_header_items.display_order ASC')
   belongs_to :checklist_row, foreign_key: :checklist_row_id, class_name: "ChecklistRow"
   belongs_to :checklist_header_item, foreign_key: :checklist_header_item_id, class_name: "ChecklistHeaderItem"
 
