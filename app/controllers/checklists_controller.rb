2006bff1137130bd087e6829700cd503d98271af
Samir Matin <samir.matin@prodigiq.com>
2019-10-22 23:02:05 +0000

    Fixes N+1 queries when rendering Checklist show

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -68,7 +68,10 @@ class ChecklistsController < ApplicationController
 
   def show
     @fields = @table.get_meta_fields('show')
-    @record = @table.find(params[:id])
+    @record = @table.includes(
+      checklist_rows: { checklist_cells: :checklist_header_item },
+      checklist_header: :checklist_header_items,
+    ).find(params[:id])
   end
 
 
