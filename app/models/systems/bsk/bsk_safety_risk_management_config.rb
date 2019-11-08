ceb9a2138a95c1593220f03c621df3356034484f
Dylan Januzik <dylan.januzik@prodigiq.com>
2019-11-08 23:58:13 +0000

    Client-specific action access definitions brought into new configs

--- app/models/systems/bsk/bsk_safety_risk_management_config.rb
+++ app/models/systems/bsk/bsk_safety_risk_management_config.rb
@@ -8,12 +8,12 @@ class BSKSafetyRiskManagementConfig < DefaultSafetyRiskManagementConfig
     [
       { field: "get_id",                    title: "ID",                                       num_cols: 6,   type: "text",        visible: 'index,show',        required: false},
       { field: "status",                    title: "Status",                                   num_cols: 6,   type: "text",        visible: 'index,show',        required: false},
-      { field: 'get_source',                title: 'Source of Input',                           num_cols: 6,   type: 'text',         visible: 'index,show',      required: false},
+      { field: 'get_source',                title: 'Source of Input',                          num_cols: 6,   type: 'text',        visible: 'index,show',        required: false},
       {                                                                                                       type: "newline",     visible: 'form,show'},
       { field: "title",                     title: "SRA Title",                                num_cols: 6,   type: "text",        visible: 'index,form,show',   required: false},
-      { field: "type_of_change",            title: "Reason for Change",                           num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: Sra.get_custom_options('SRA Type of Change')},
+      { field: "type_of_change",            title: "Reason for Change",                        num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: Sra.get_custom_options('SRA Type of Change')},
       { field: "system_task",               title: "System/Task",                              num_cols: 6,   type: "datalist",    visible: 'index,form,show',   required: false, options: Sra.get_custom_options('Systems/Tasks')},
-      { field: "responsible_user_id",       title: "Responsible User",                         num_cols: 6,   type: "user",        visible: 'index,form,show',  required: false},
+      { field: "responsible_user_id",       title: "Responsible User",                         num_cols: 6,   type: "user",        visible: 'index,form,show',   required: false},
       { field: "reviewer_id",               title: "Quality Reviewer",                         num_cols: 6,   type: "user",        visible: 'form,show',         required: false},
       { field: "approver_id",               title: "Final Approver",                           num_cols: 6,   type: "user",        visible: 'form,show',         required: false},
       { field: "scheduled_completion_date", title: "Scheduled Completion Date",                num_cols: 6,   type: "date",        visible: 'index,form,show',   required: false},
@@ -22,7 +22,7 @@ class BSKSafetyRiskManagementConfig < DefaultSafetyRiskManagementConfig
       {                                     title: "Affected Department",                      num_cols: 12,  type: "panel_start", visible: 'form,show'},
       { field: "departments",               title: "Affected Departments",                     num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: Sra.get_custom_options('Departments')},
       { field: "other_department",          title: "Other Affected Departments",               num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
-      { field: "departments_comment",       title: "Affected Departments Comments",            num_cols: 12,  type: "textarea",    visible: 'form,show',        required: false},
+      { field: "departments_comment",       title: "Affected Departments Comments",            num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
       {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
 
       {                                     title: "Affected Programs",                        num_cols: 12,  type: "panel_start", visible: 'form,show'},
@@ -41,7 +41,7 @@ class BSKSafetyRiskManagementConfig < DefaultSafetyRiskManagementConfig
       { field: "compliances",               title: "Affected System Task Analysis SHEL(L) Models",           num_cols: 12,  type: "checkbox",    visible: 'form,show',         required: false, options: Sra.get_custom_options("System Task Analysis SHEL(L) Models")},
       { field: "other_compliance",          title: "Other Affected System Task Analysis SHEL(L) Models",     num_cols: 6,   type: "text",        visible: 'form,show',         required: false},
       { field: "compliances_comment",       title: "Affected System Task Analysis SHEL(L) Models Comments",  num_cols: 12,  type: "textarea",    visible: 'form,show',         required: false},
-      {                                                                                       num_cols: 12,  type: "panel_end",   visible: 'form,show'},
+      {                                                                                        num_cols: 12,  type: "panel_end",   visible: 'form,show'},
 
       { field: "closing_comment",           title: "Responsible User's Closing Comments",      num_cols: 12,  type: "text",        visible: 'show'},
       { field: "reviewer_comment",          title: "Quality Reviewer's Closing Comments",      num_cols: 12,  type: "text",        visible: 'show'},
@@ -49,4 +49,19 @@ class BSKSafetyRiskManagementConfig < DefaultSafetyRiskManagementConfig
     ]
   end
 
+
+  HIERARCHY = DefaultSafetyRiskManagementConfig::HIERARCHY.deep_merge({
+    objects:{
+      'Hazard' => {
+        actions: {
+          complete: {
+            access: proc { |owner:,user:,**op|
+              # Request for Hazards to not be completed without root cause analysis - 11/2019 Armando Martinez
+              super_proc('Hazard',:complete).call(owner:owner,user:user,**op) && owner.root_causes.present?
+            },
+          },
+        },
+      },
+    },
+  })
 end
--- app/models/systems/scx/scx_safety_reporting_config.rb
+++ app/models/systems/scx/scx_safety_reporting_config.rb
@@ -6,4 +6,51 @@ class SCXSafetyReportingConfig < DefaultSafetyReportingConfig
     # Airline-Specific Features:
   })
 
+  HIERARCHY = DefaultSafetyAssuranceConfig::HIERARCHY.deep_merge({
+    objects:{
+      'Submission' => {
+        actions: {
+          pdf: {
+            access: proc { |owner:,user:,**op|
+              # Requested only admins can use normal pdf function 11/8/2019 by Stephanie Gabert
+              super_proc('Submission',:pdf).call(owner:owner,user:user,**op) && user.admin?
+            },
+          },
+        },
+      },
+      'Record' => {
+        actions: {
+          pdf: {
+            access: proc { |owner:,user:,**op|
+              # Requested only admins can use normal pdf function 11/8/2019 by Stephanie Gabert
+              super_proc('Record',:pdf).call(owner:owner,user:user,**op) && user.admin?
+            },
+          },
+        },
+      },
+      'Report' => {
+        actions: {
+          pdf: {
+            access: proc { |owner:,user:,**op|
+              # Requested only admins can use normal pdf function 11/8/2019 by Stephanie Gabert
+              super_proc('Report',:pdf).call(owner:owner,user:user,**op) && user.admin?
+            },
+          },
+          meeting_ready: {
+            access: proc { |owner:,user:,**op|
+              # Requested unavailable until root cause analysis 10/2019 by Stephanie Gabert
+              super_proc('Report',:pdf).call(owner:owner,user:user,**op) && owner.root_causes.present?
+            },
+          },
+          close: {
+            access: proc { |owner:,user:,**op|
+              # Requested unavailable until root cause analysis 10/2019 by Stephanie Gabert
+              super_proc('Report',:pdf).call(owner:owner,user:user,**op) && owner.root_causes.present?
+            },
+          },
+        },
+      },
+    },
+  })
+
 end
