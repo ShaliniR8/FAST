3aacbde3f688a26a41a43c19c06a10032ca8bf39
Blair Li <blair.li@prodigiq.com>
2019-12-17 01:45:39 +0000

    Adds migration to map Query.visualizations to QueryVisualization

--- db/migrate/20191216235514_remove_visualizations_from_queries.rb
+++ db/migrate/20191216235514_remove_visualizations_from_queries.rb
@@ -1,19 +1,14 @@
 class RemoveVisualizationsFromQueries < ActiveRecord::Migration
   def self.up
-    #rename_column :queries, :visualizations, :old_vis
+    rename_column :queries, :visualizations, :old_vis
     Query.all.each do |query|
-      puts "here"
       (query.old_vis || []).each do |vis|
-        QueryVisualization.create({
-          owner_id: query.id,
-          x_axis: vis,
-        }) if vis.present?
+        QueryVisualization.create({owner_id: query.id, x_axis: vis}) if vis.present?
       end
     end
-    #remove_column :queries, :visualizations
   end
 
   def self.down
-    #rename_column :queries, :old_vis, :visualizations
+    rename_column :queries, :old_vis, :visualizations
   end
 end
