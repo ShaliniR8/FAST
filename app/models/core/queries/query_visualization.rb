615e3548f68db0efe621e634a114a8aaba600ab5
Blair Li <blair.li@prodigiq.com>
2019-12-20 20:47:57 +0000

    Adds default options to visualzation charts

--- app/models/core/queries/query_visualization.rb
+++ app/models/core/queries/query_visualization.rb
@@ -3,40 +3,55 @@ class QueryVisualization < ActiveRecord::Base
   belongs_to :query, foreign_key: :owner_id, class_name: 'Query'
 
 
-  # def self.chart_types
-  #   {
-  #     'Grid'    => 1,
-  #     'Pie'     => 2,
-  #     'Column'  => 3,
-  #     'Line'    => 4,
-  #     'Stacked' => 5,
-  #   }
-  # end
+  DEFAULT_OPTIONS = {
+    height: 400,
+    legend: {position: 'top'},
+    hAxis: {
+      textStyle: {fontSize: 14}
+    },
+    titleTextStyle: {
+      fontSize: 18,
+      bold: true,
+      fontName: "Quattrocento Sans"
+    },
+    tooltip: {
+      textStyle: {
+        fontSize: 14,
+        fontName: "Quattrocento Sans"
+      },
+      showColorCode: true,
+    }
+  }
 
 
   def self.chart_types
     [
-      {val: 1, chart_name: 'Grid View',     options: {}},
-      {val: 2, chart_name: 'Pie Chart',     options:  {
-                                                        height: 400,
-                                                        legend: {position: 'labeled'},
-                                                        pieSliceText: 'annotationText',
-                                                        tooltip: {textStyle: {color: 'black'}, showColorCode: true},
-                                                      }},
-      {val: 3, chart_name: 'Column Chart',  options: {
-                                                        height: 400,
-                                                        legend: { position: "top"},
-                                                      }},
-      {val: 4, chart_name: 'Line Chart',    options: {
-                                                        chartArea: {height: "40%", width: "95%" },
-                                                        legend: { position: "top" },
-                                                        height: 400,
-                                                      }},
-      {val: 5, chart_name: 'Stacked Chart', options:  {
-                                                        isStacked: true,
-                                                        legend: { position: 'top', maxLines: 3 },
-                                                        height: 400,
-                                                      }},
+      {val: 1, chart_name: 'Grid',     options: {}},
+      {val: 2, chart_name: 'Pie Chart',
+        options:  DEFAULT_OPTIONS.deep_merge({
+          legend: {position: 'labeled'},
+          pieSliceText: 'annotationText',
+          tooltip: {
+            textStyle: {color: 'black'},
+            showColorCode: true},
+        }),
+      },
+      {val: 3, chart_name: 'Column Chart',
+        options: DEFAULT_OPTIONS.deep_merge({
+          seriesType: 'bars',
+        }),
+      },
+      {val: 4, chart_name: 'Line Chart',
+        options: DEFAULT_OPTIONS.deep_merge({
+          seriesType: 'line',
+        }),
+      },
+      {val: 5, chart_name: 'Stacked Chart',
+        options: DEFAULT_OPTIONS.deep_merge({
+          isStacked: true,
+          legend: {maxLines: 3},
+        }),
+      },
     ]
   end
 
