782b049de60b1585cd248a7000f247aeaf9304ae
Blair Li <blair.li@prodigiq.com>
2020-01-01 01:02:40 +0000

    Upgrades TMC's checklist Version 3

--- app/models/systems/tmc/tmc_safety_assurance_config.rb
+++ app/models/systems/tmc/tmc_safety_assurance_config.rb
@@ -2,7 +2,7 @@ class TMCSafetyAssuranceConfig < DefaultSafetyAssuranceConfig
 
   GENERAL = DefaultSafetyAssuranceConfig::GENERAL.merge({
     # General Module Features:
-    checklist_version:                  '1',
+    checklist_version:                  '3',
     enable_recurrence:                  false,
 
     # Airline-Specific Features:
