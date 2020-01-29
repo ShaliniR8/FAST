d6a7f3449eca19eaa814e6bfcf3d4944914866d1
Blair Li <blair.li@prodigiq.com>
2020-01-29 19:01:25 +0000

    Moves CONFIG initialization to environment to accomadate I18n load

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,24 @@ require 'carrierwave/orm/activerecord'
 
 # Initialize the rails application
 PrdgSession::Application.initialize!
+
+
+unless Object.const_defined? 'CONFIG'
+  CONFIG ||= Object.const_get("#{AIRLINE_CODE}Config")
+end
+
+unless Object.const_defined? 'CONFIG_IM'
+  CONFIG_IM ||= Object.const_get("#{AIRLINE_CODE}ImplementationManagementConfig") rescue DefaultImplementationManagementConfig
+end
+
+unless Object.const_defined? 'CONFIG_SA'
+  CONFIG_SA ||= Object.const_get("#{AIRLINE_CODE}SafetyAssuranceConfig") rescue DefaultSafetyAssuranceConfig
+end
+
+unless Object.const_defined? 'CONFIG_SRM'
+  CONFIG_SRM ||= Object.const_get("#{AIRLINE_CODE}SafetyRiskManagementConfig") rescue DefaultSafetyRiskManagementConfig
+end
+
+unless Object.const_defined? 'CONFIG_SR'
+  CONFIG_SR ||= Object.const_get("#{AIRLINE_CODE}SafetyReportingConfig") rescue DefaultSafetyReportingConfig
+end
