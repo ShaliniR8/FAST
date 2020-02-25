2e39fde326457e68febd543973dc26d347182682
Blair Li <blair.li@prodigiq.com>
2020-02-25 01:49:29 +0000

    Fixes recurring audit frequency dropdown not showing issue

--- app/models/sa/recurrence.rb
+++ app/models/sa/recurrence.rb
@@ -8,7 +8,7 @@ class Recurrence < ActiveRecord::Base
       {field: 'id',           title: 'ID',                num_cols: 6, type: 'text',          visible: 'index,show',       required: false},
       {field: 'title',        title: 'Recurrence Title',  num_cols: 6, type: 'text',          visible: 'index,form,show',  required: true},
       {field: 'form_type',    title: 'Type',              num_cols: 6, type: 'text',          visible: 'show',             required: false},
-      {field: 'frequency',    title: 'Frequency',         num_cols: 6, type: 'select',        visible: 'index,form,show',  required: true, options: get_frequency},
+      {field: 'frequency',    title: 'Frequency',         num_cols: 6, type: 'select',        visible: 'index,form,show',  required: true, options: "Recurrence.get_frequency"},
       {field: 'next_date',    title: 'Next Creation Date',num_cols: 6, type: 'date',          visible: 'index,form,show',  required: true},
       {field: 'end_date',     title: 'End Date',          num_cols: 6, type: 'date',          visible: 'index,form,show',  required: false},
     ].select{|f| (f[:visible].split(',') & visible_fields).any?}
