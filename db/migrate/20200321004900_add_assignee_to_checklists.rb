8204160748086fd0811b57402852d32a8249b735
Blair Li <blair.li@prodigiq.com>
2020-03-23 22:41:18 +0000

    Updates assignee column name in checklist

--- db/migrate/20200321004900_add_assignee_to_checklists.rb
+++ db/migrate/20200321004900_add_assignee_to_checklists.rb
@@ -1,9 +1,9 @@
 class AddAssigneeToChecklists < ActiveRecord::Migration
   def self.up
-  	add_column :checklists, :assignees, :string
+  	add_column :checklists, :assignee_ids, :string
   end
 
   def self.down
-  	remove_column :checklists, :assignees
+  	remove_column :checklists, :assignee_ids
   end
 end
