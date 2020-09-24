625fc863fd4bb195fd1c78b2a704d13bf7a9f76a
Trevor Ryles <trevor.ryles@prodigiq.com>
2020-09-24 20:18:36 +0000

    FFT Request: Adds Datalist for Event Title that uses Custom Option 'Event Titles'

--- app/models/systems/fft/fft_safety_reporting_config.rb
+++ app/models/systems/fft/fft_safety_reporting_config.rb
@@ -11,5 +11,18 @@ class FFTSafetyReportingConfig < DefaultSafetyReportingConfig
     # Airline-Specific Features:
   })
 
+  HIERARCHY = DefaultSafetyReportingConfig::HIERARCHY.deep_merge({
+    objects:{
+      'Report' => {
+        fields: {
+          name: {
+            field: 'name', title: 'Event Title',
+            num_cols: 6, type: 'datalist', visible: 'index,form,meeting_form,show',
+            required: true, on_newline: true, options: CONFIG.custom_options['Event Titles']
+          }
+        }
+      }
+    }
+  })
 
 end
