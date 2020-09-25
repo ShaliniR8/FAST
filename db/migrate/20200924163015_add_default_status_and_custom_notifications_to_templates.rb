26e739a30ee1979a756839c59d1a11120db9ee06
Trevor Ryles <trevor.ryles@prodigiq.com>
2020-09-25 16:43:36 +0000

    Adds creation of 'Event Titles' Custom Option to migration

--- db/migrate/20200924163015_add_default_status_and_custom_notifications_to_templates.rb
+++ db/migrate/20200924163015_add_default_status_and_custom_notifications_to_templates.rb
@@ -3,11 +3,18 @@ class AddDefaultStatusAndCustomNotificationsToTemplates < ActiveRecord::Migratio
     add_column :templates, :default_status, :string
     add_column :templates, :submitter_message, :text
     add_column :templates, :notifier_message, :text
+    if !CustomOption.where(title: 'Event Titles').present?
+      CustomOption.create(title: 'Event Titles', description: 'This manages all the Event Titles in the System')
+    end
   end
 
   def self.down
     remove_column :templates, :notifier_message
     remove_column :templates, :submitter_message
     remove_column :templates, :default_status
+    event_titles = CustomOption.where(title: 'Event Titles')
+    if event_titles.present?
+      event_titles.each(&:destroy)
+    end
   end
 end
