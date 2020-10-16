8d48fe5ef466cbcb1b60933394dd54729c1bf406
Tae Ho <taeho.kim@prodigiq.com>
2020-10-16 19:03:59 +0000

    Use BOE risk matrix for Demo portal

--- app/models/systems/demo/demo_config.rb
+++ app/models/systems/demo/demo_config.rb
@@ -21,12 +21,38 @@ class DemoConfig < DefaultConfig
     allow_reopen_report:          false,
     configurable_risk_matrices:   true,
     shared_links:                 true,
-    drop_down_risk_selection:     true,
+    drop_down_risk_selection:     false,
 
     # TO BE REMOVED:
     allow_set_alert:              true,
     has_extension:                true,
     has_verification:             true,
+  })
+
+  MATRIX_INFO = DefaultConfig::MATRIX_INFO.deep_merge({
+    severity_table: {
+      row_header: ['5','4','3','2','1'],
+    },
+    probability_table: {
+      column_header: ['A','B','C','D','E'],
+    },
+    risk_table: {
+      row_header: ['5','4','3','2','1'],
+      column_header: ['A','B','C','D','E'],
+      rows_color: [
+        ['yellow','red','red','red','red'],
+        ['yellow','yellow','red','red','red'],
+        ['limegreen','yellow','yellow','yellow','red'],
+        ['limegreen','limegreen','yellow','yellow','yellow'],
+        ['limegreen','limegreen','limegreen','yellow','yellow']
+      ],
+    },
+  }).merge({ # replace default risk_definitions with boe custom definitions
+    risk_definitions: {
+      red:       {rating: "HIGH",     cells: "A4, A3, B4",     description: "Unacceptable"                 },
+      yellow:    {rating: "MODERATE", cells: "A2, B2, C4",     description: "Acceptable with Mitigation"   },
+      limegreen: {rating: "LOW",      cells: "A1, B2, C3, D4", description: "Acceptable"                   },
+    },
   })
 
   FAA_INFO = DefaultConfig::FAA_INFO.merge({
