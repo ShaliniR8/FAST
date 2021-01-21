7287477578e275df70a63805bfc3f265eab50f98
Saptarshi Chatterjee <saptarshi.chatterjee@prodigiq.com>
2021-01-21 23:14:33 +0000

    Fix typo in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -920,7 +920,7 @@ class ApplicationController < ActionController::Base
 
 
   def filter_records(object_name, controller_name)
-    if %w[Aduit Inspection Evaluation Investigation].include? object_name
+    if %w[Audit Inspection Evaluation Investigation].include? object_name
       @records = @records.keep_if{|x| x[:template].nil? || !x[:template]}
       if !current_user.has_access(controller_name,'admin', admin: true, strict: true)
         cars =  Object.const_get(object_name).where('status in (?) and responsible_user_id = ?',
