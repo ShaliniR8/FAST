e6be6d0f4f6f1ff2bd33ab659128302975ac49d4
Saptarshi Chatterjee <saptarshi.chatterjee@prodigiq.com>
2021-05-14 16:52:16 +0000

    Enables SA module in mobile app for RVF

--- app/models/systems/rvf/rvf_config.rb
+++ app/models/systems/rvf/rvf_config.rb
@@ -5,7 +5,7 @@ class RVFConfig < DefaultConfig
   #used for creating different environments in database.yml; example would be %w[training]
   SYSTEM_ENVIRONMENTS = %w[training]
 
-  MOBILE_MODULES = %w[ASAP]
+  MOBILE_MODULES = %w[ASAP SMS]
 
   GENERAL = DefaultConfig::GENERAL.merge({
     # AIRLINE-SPECIFIC CONFIGS
