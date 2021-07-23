8b2bdd116fb9f9946642decc947f584d7a273174
Saptarshi Chatterjee <saptarshi.chatterjee@prodigiq.com>
2021-07-23 17:40:54 +0000

    Rejection by pending approver or quality reviewer will now log comments in the transaction log

--- app/controllers/sras_controller.rb
+++ app/controllers/sras_controller.rb
@@ -152,7 +152,7 @@ class SrasController < ApplicationController
             content: "SRA ##{@owner.id} was Rejected by the Quality Reviewer."},
           mailer: true,
           subject: 'SRA Rejected') if @owner.responsible_user
-        transaction_content = 'Rejected by the Quality Reviewer'
+        transaction_content = "Rejected by the Quality Reviewer with comment #{params[:sra][:reviewer_comment] rescue ""}"
       else
         update_status = 'Assigned'
         notify(@owner,
@@ -161,7 +161,7 @@ class SrasController < ApplicationController
             content: "SRA ##{@owner.id} was Rejected by the Final Approver."},
           mailer: true,
           subject: 'SRA Rejected') if @owner.responsible_user
-        transaction_content = 'Rejected by the Final Approver'
+        transaction_content = "Rejected by the Final Approver with comment #{params[:sra][:approver_comment] rescue ""}"
       end
     when 'Approve'
       # if !@owner.approver #Approved by reviewer with absent approver case
