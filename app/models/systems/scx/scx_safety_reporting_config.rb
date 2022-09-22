9ac467ee630866ee1df2ff7ffe8657c8cf5187a7
Taeho <taeho.kim@prodigiq.com>
2022-09-22 21:06:25 +0000

    Update SCX attachments access control in event page

--- app/models/systems/scx/scx_safety_reporting_config.rb
+++ app/models/systems/scx/scx_safety_reporting_config.rb
@@ -46,7 +46,7 @@ class SCXSafetyReportingConfig < DefaultSafetyReportingConfig
           },
           event_station: {
             field: 'event_station', title: 'Event Station',
-            num_cols: 4, type: 'select', options: "CONFIG.custom_options['Station Codes']", visible: 'index,form,meeting_form,show',
+            num_cols: 4, type: 'datalist', options: "CONFIG.custom_options['Station Codes']", visible: 'index,form,meeting_form,show',
           },
           event_date: {
             default: true, title: 'Event Date',
