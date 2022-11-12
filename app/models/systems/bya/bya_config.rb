8cabf1c5fb21217272b3e367386e5749189de2a6
Taeho <taeho.kim@prodigiq.com>
2022-11-12 01:15:07 +0000

    Update BYA risk matrix

--- app/models/systems/bya/bya_config.rb
+++ app/models/systems/bya/bya_config.rb
@@ -34,34 +34,37 @@ class BYAConfig < DefaultConfig
 
   MATRIX_INFO = {
     terminology: {
-      baseline_btn: 'Initial Risk',
+      baseline_btn: 'Baseline Risk',
       mitigate_btn: 'Mitigate Risk',
       'Baseline' => 'Baseline',
       'Mitigate' => 'Mitigated'
     },
 
     severity_table: {
-      title: 'SEVERITY',
+      title: 'SEVERITY EXERCISE',
 
       orientation: :horizontal,
-      direction: :left,
-      size: 'col-xs-6',
+      direction: :right,
+      size: 'col-xs-8',
       title_style: 'severityTitle',
       main_header_style: 'sevMainHeader',
       header_style: 'sevHeader',
       cell_name: 'severity_td',
 
       column_header_name: 'SEVERITY',
-      column_header: ['1 - Negligible', '2 - Minor', '3 - Major', '4 - Hazardous', '5 - Catastrophic'],
-      row_header_name: 'CLASS',
+      column_header: ['1','2','3','4','5'],
+      row_header_name: ' ',
       row_header: [
-        'SELECT SEVERITY'
+        'CLASS',
       ],
       rows: [
-        [ #System/  Program/ Policy
-          'Negligible', 'Minor', 'Major', 'Hazardous', 'Catastrophic'
+        [ 
+          'Negligible',
+          'Minor',
+          'Major',
+          'Hazardous',
+          'Catastrophic'
         ]
-
       ] #End of rows
     },
 
@@ -74,51 +77,41 @@ class BYAConfig < DefaultConfig
     },
 
     probability_table: {
-      title: 'LIKELIHOOD',
+      title: 'PROBABILITY EXERCISE',
 
       orientation: :vertical,
       direction: :up,
-      size: 'col-xs-6',
+      size: 'col-xs-4',
       title_style: 'probabilityTitle',
       main_header_style: 'probMainHeader',
       header_style: 'probHeader',
       cell_name: 'probability_td',
 
       row_header_name: 'PROBABILITY',
-      row_header: ['5 - Frequent', '4 - Occasional', '3 - Remote', '2 - Improbable', '1 - Extremely Improbable'],
-      column_header_name: 'CRITERIA',
-      column_header: [''],
+      row_header: ['5', '4', '3', '2', '1'],
+      column_header_name: '',
+      column_header: ['CLASS'],
       rows: [
-        [ #1
-          "Frequent"
-        ],
-        [ #2
-          "Occasional"
-        ],
-        [ #3
-          "Remote"
-        ],
-        [ #4
-          "Improbable"
-        ],
-        [ #5
-          "Extremely Improbable"
-        ]
-      ] #End of rows
+        ['Frequent'],
+        ['Occasional'],
+        ['Remote'],
+        ['Improbable'],
+        ['Extremely Improbable'],
+      ]
     },
 
     probability_table_dict: {
-      0 => '4',
-      1 => '3',
-      2 => '2',
-      3 => '1',
-      4 => '0'
+      0 => 'Frequent 5',
+      1 => 'Occasional 4',
+      2 => 'Remote 3',
+      3 => 'Improbable 2',
+      4 => 'Extremely Improbable 1',
     },
 
     risk_table: {
       title: 'RISK ASSESSMENT MATRIX',
 
-      size: 'col-xs-6',
+      size: 'col-xs-8',
       title_style: 'matrixTitle',
       main_header_style: 'matrixMainHeader',
       header_style: 'matrixHeader',
@@ -131,7 +124,7 @@ class BYAConfig < DefaultConfig
 
       column_header_name: 'SEVERITY',
       column_header: ['1 - Negligible', '2 - Minor', '3 - Major', '4 - Hazardous', '5 - Catastrophic'],
-      row_header_name: 'LIKELIHOOD',
+      row_header_name: 'PROBABILITY',
       row_header: ['5 - Frequent', '4 - Occasional', '3 - Remote', '2 - Improbable', '1 - Extremely Improbable'],
 
       rows_color: [
@@ -140,14 +133,6 @@ class BYAConfig < DefaultConfig
         ["mediumseagreen",  "mediumseagreen", "yellow",         "yellow",         "red"],
         ["mediumseagreen",  "mediumseagreen", "mediumseagreen", "yellow",         "yellow"],
         ["mediumseagreen",  "mediumseagreen", "mediumseagreen", "mediumseagreen", "yellow"],
-      ],
-
-      rows: [
-        ["Yellow - Acceptable with Mitigation",  "Yellow - Acceptable with Mitigation",   "Red- Unacceptable",                     "Red- Unacceptable",                     "Red- Unacceptable"],
-        ["Green - Acceptable",                   "Yellow - Acceptable with Mitigation",   "Yellow - Acceptable with Mitigation",   "Red- Unacceptable",                     "Red- Unacceptable"],
-        ["Green - Acceptable",                   "Green - Acceptable",                    "Yellow - Acceptable with Mitigation",   "Yellow - Acceptable with Mitigation",   "Red- Unacceptable"],
-        ["Green - Acceptable",                   "Green - Acceptable",                    "Green - Acceptable",                    "Yellow - Acceptable with Mitigation",   "Yellow - Acceptable with Mitigation"],
-        ["Green - Acceptable",                   "Green - Acceptable",                    "Green - Acceptable",                    "Green - Acceptable",                    "Yellow - Acceptable with Mitigation"],
       ],
     },
 
@@ -164,9 +149,9 @@ class BYAConfig < DefaultConfig
     },
 
     risk_definitions: {
-      red:              { rating: 'High',      cells: 'A/1, A/2, and B/1',                        description: 'Unacceptable' },
+      mediumseagreen:   { rating: 'Low',       cells: 'A/5, B/5, C/4, C/5, D/3, D/4, and D/5',    description: 'Acceptable' },
       yellow:           { rating: 'Moderate',  cells: 'A/4, B/3, and D/1',                        description: 'Acceptable with Mitigation' },
-      mediumseagreen:   { rating: 'Low',       cells: 'A/5, B/5, C/4, C/5, D/3, D/4, and D/5',    description: 'Acceptable' }
+      red:              { rating: 'High',      cells: 'A/1, A/2, and B/1',                        description: 'Unacceptable' }
     },
   }
 
