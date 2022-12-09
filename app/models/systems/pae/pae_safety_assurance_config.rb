0951ca8bb4216224a462902675e9005f4e8af354
Saptarshi Chatterjee <saptarshi.chatterjee@prodigiq.com>
2022-12-09 00:22:58 +0000

    Add recurrence options to PAE SA module

---
+++ app/models/systems/pae/pae_safety_assurance_config.rb
@@ -0,0 +1,13 @@
+class PAESafetyAssuranceConfig < DefaultSafetyAssuranceConfig
+
+  GENERAL = DefaultSafetyAssuranceConfig::GENERAL.merge({
+    # General Module Features:
+    checklist_version:                  '3',
+    recurring_item_checklist:           true,
+    non_recurring_item_checklist:       true,
+    days_to_complete_instead_of_date:   true,
+    daily_weekly_recurrence_frequecies: true,
+  })
+
+
+end
