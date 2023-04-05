610c5b8f9bf2de2155a873da96e41dfc7f0b7b2d
Shalini Roy <shalini.roy@prodigiq.com>
2023-04-05 16:46:31 -0700

    Time zone button toggle issue fixed

--- app/models/systems/atn/atn_safety_reporting_config.rb
+++ app/models/systems/atn/atn_safety_reporting_config.rb
@@ -3,6 +3,7 @@ class ATNSafetyReportingConfig < DefaultSafetyReportingConfig
   GENERAL = DefaultSafetyReportingConfig::GENERAL.merge({
     # General Module Features:
     submission_time_zone:            false,
+    submission_local_time_zone:      true,
     # Airline-Specific Features:
     attach_pdf_submission:           'none',      # 1: ided (identified pdf), 2: deid (deidentified pdf), 3: none (no pdf attachment)
     match_submission_record_id:      true,
