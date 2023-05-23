defff3df0f6e9b3daea0980dfbfcd4d98bcbe66e
Saptarshi Chatterjee <saptarshi.chatterjee@prodigiq.com>
2023-05-23 00:24:34 +0000

    FIX TMC Jets config file

--- app/models/systems/tmc/tmc_safety_risk_management_config.rb
+++ app/models/systems/tmc/tmc_safety_risk_management_config.rb
@@ -1,4 +1,4 @@
-class SJOSafetyRiskManagementConfig < DefaultSafetyRiskManagementConfig
+class TMCSafetyRiskManagementConfig < DefaultSafetyRiskManagementConfig
   GENERAL = DefaultSafetyRiskManagementConfig::GENERAL.merge({
     enable_risk_register:     false,
   })
