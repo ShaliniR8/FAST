21ecc6e2daca5a2bbb704727f4544027e74a941a
user.email <karan.pardasani@prodigiq.com>
2023-08-15 17:33:01 -0700

    Updated/removed fields for Investigations and SmsActions for BAH

--- app/models/systems/bah/bah_safety_assurance_config.rb
+++ app/models/systems/bah/bah_safety_assurance_config.rb
@@ -4,4 +4,61 @@ class BAHSafetyAssuranceConfig < DefaultSafetyAssuranceConfig
     daily_weekly_recurrence_frequecies: true,
   })
 
+  HIERARCHY = DefaultSafetyAssuranceConfig::HIERARCHY.deep_merge({
+    objects:{
+      'Investigation' => {
+        fields: {
+          local_event_occurred: {
+            field: 'local_event_occured', title: 'Time when investigation is launched',
+            num_cols: 6, type: 'datetime', visible: 'form,show',
+            required: false
+          },
+          ntsb: {
+            field: 'ntsb', title: 'NTSB Reportable',
+            num_cols: 6, type: 'boolean_box', visible: '',
+            required: false
+          },
+          safety_hazard: {
+            field: 'safety_hazard', title: 'Safety Hazard',
+            num_cols: 6, type: 'boolean_box', visible: '',
+            required: false
+          },
+          containment: {
+            field: 'containment', title: 'Containment',
+            num_cols: 12, type: 'textarea', visible: '',
+            required: false
+          },
+        }
+      },
+      'SmsAction' => {
+        fields: {
+          emp: {
+            field: 'emp', title: 'Employee Corrective Action',
+            num_cols: 6, type: 'boolean_box', visible: '',
+            required: false, on_newline: true
+          },
+          dep: {
+            field: 'dep', title: 'Company Corrective Action',
+            num_cols: 6, type: 'boolean_box', visible: '',
+            required: false
+          },
+          immediate_action: {
+            field: 'immediate_action', title: 'Immediate Action',
+            num_cols: 6, type: 'boolean_box', visible: '',
+            required: false, on_newline: true
+          },
+          comprehensive_action: {
+            field: 'comprehensive_action', title: 'Comprehensive Action',
+            num_cols: 6, type: 'boolean_box', visible: '',
+            required: false
+          },
+          comprehensive_action_comment: {
+            field: 'comprehensive_action_comment', title: 'Comprehensive Action Comment',
+            num_cols: 12, type: 'textarea', visible: '',
+            required: false
+          },
+        }
+      }
+    }
+  })
 end
