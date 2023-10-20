6a6afc91b92bb46b218873a37e1934e65580f247
Shalini Roy <shalini.roy@prodigiq.com>
2023-10-20 15:32:52 -0700

    default local timezone for submission

--- app/models/systems/ruh/ruh_safety_reporting_config.rb
+++ app/models/systems/ruh/ruh_safety_reporting_config.rb
@@ -4,7 +4,7 @@ class RUHSafetyReportingConfig < DefaultSafetyReportingConfig
     # General Module Features:
     show_submitter_name:              true,
     enable_dual_report:               true,
-    submission_time_zone:             true,
+    submission_local_time_zone:       true,
     submission_description_required:  true,
     configurable_agenda_dispositions: true,
     # Airline-Specific Features:
